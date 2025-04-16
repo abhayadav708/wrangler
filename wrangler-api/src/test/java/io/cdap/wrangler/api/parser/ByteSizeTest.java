627eda34dc32359715f1bd4b10faf8fd7be4ba63
Abhay Kumar Yadav <118895097+abhayadav708@users.noreply.github.com>
2025-04-16 19:30:47 +0530

    Update ByteSizeTest.java

--- wrangler-api/src/test/java/io/cdap/wrangler/api/parser/ByteSizeTest.java
+++ wrangler-api/src/test/java/io/cdap/wrangler/api/parser/ByteSizeTest.java
@@ -1,1 +1,21 @@
+package io.cdap.wrangler.api.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ByteSizeTest {
+
+  @Test
+  public void testParsing() {
+    Assert.assertEquals(1024, new ByteSize("1KB").getBytes());
+    Assert.assertEquals(1048576, new ByteSize("1MB").getBytes());
+    Assert.assertEquals(10 * 1024, new ByteSize("10KB").getBytes());
+    Assert.assertEquals(2L * 1024 * 1024 * 1024, new ByteSize("2GB").getBytes());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidInput() {
+    new ByteSize("123XY");
+  }
+}
 
