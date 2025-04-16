72dc0bdadf42301f6f3f616e15e362edda274bde
Abhay Kumar Yadav <118895097+abhayadav708@users.noreply.github.com>
2025-04-16 19:31:43 +0530

    Update TimeDurationTest.java

--- wrangler-api/src/test/java/io/cdap/wrangler/api/parser/TimeDurationTest.java
+++ wrangler-api/src/test/java/io/cdap/wrangler/api/parser/TimeDurationTest.java
@@ -1,1 +1,21 @@
+package io.cdap.wrangler.api.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TimeDurationTest {
+
+  @Test
+  public void testParsing() {
+    Assert.assertEquals(1000, new TimeDuration("1s").getMillis());
+    Assert.assertEquals(1500, new TimeDuration("1500ms").getMillis());
+    Assert.assertEquals(60000, new TimeDuration("1m").getMillis());
+    Assert.assertEquals(3600000, new TimeDuration("1h").getMillis());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidInput() {
+    new TimeDuration("5lightyears");
+  }
+}
 
